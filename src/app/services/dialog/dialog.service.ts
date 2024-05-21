45c6a6c412214c483ff7963d1329e36bc2935044
kaitbunny <patrikogit@gmail.com>
2024-05-21 08:52:24 -0300

    feat: criacao do service de Dialog

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Dialog } from '@capacitor/dialog';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,27 @@ import { Injectable } from '@angular/core';
 export class DialogService {
 
   constructor() { }
+
+  public static async alert(title: string, message: string) {
+    await Dialog.alert({
+      title: title,
+      message: message
+    });
+  }
+
+  public static async confirm(title: string, message: string): Promise<boolean> {
+    const { value } = await Dialog.confirm({
+      title: title,
+      message: message
+    });
+    return value;
+  }
+
+  public static async prompt(title: string, message: string): Promise<string> {
+    const { value } = await Dialog.prompt({
+      title: title,
+      message: message
+    });
+    return value;
+  }
 }
